c764f3fcffd8a0f7257d4e714250419ba43d900d
necroant <dominions3host@gmail.com>
2021-02-05 17:48:40 +0300

    Мой исходный код по состоянию на конец практического занятия 04.02.21

--- lesson_3/linked_list.py
+++ lesson_3/linked_list.py
@@ -2,21 +2,21 @@ from typing import Any, Sequence, Optional
 
 
 class LinkedList:
+    # noinspection PyUnresolvedReferences
     class Node:
         """
         Внутренний класс, класса LinkedList.
-
         Пользователь напрямую не работает с узлами списка, узлами оперирует список.
         """
-        def __init__(self, value: Any, next_: Optional['Node'] = None):
+
+        def __init__(self, value: Any, next_node: Optional["Node"] = None):
             """
             Создаем новый узел для односвязного списка
-
             :param value: Любое значение, которое помещено в узел
-            :param next_: следующий узел, если он есть
+            :param next_node: следующий узел, если он есть
             """
             self.value = value
-            self.next = next_  # Вызывается сеттер
+            self.next = next_node  # Вызывается сеттер
 
         @property
         def next(self):
@@ -24,7 +24,7 @@ class LinkedList:
             return self.__next
 
         @next.setter
-        def next(self, next_: Optional['Node']):
+        def next(self, next_: Optional["Node"]):
             """Setter проверяет и устанавливает следующий узел связного списка"""
             if not isinstance(next_, self.__class__) and next_ is not None:
                 msg = f"Устанавливаемое значение должно быть экземпляром класса {self.__class__.__name__} " \
@@ -42,64 +42,87 @@ class LinkedList:
 
     def __init__(self, data: Sequence = None):
         """Конструктор связного списка"""
-        self.len_ = 0
-        self.head = None  # Node
+        self.__len = 0
+        self.head = None  # todo реализовать getter и setter для head и tail
+        self.tail = None
 
-        if data:  # ToDo Проверить, что объект итерируемый. Метод self.is_iterable
-            for value in data:
-                self.append(value)
+        if data:
+            if self.is_iterable(data):
+                for value in data:
+                    self.append(value)
+            else:
+                self.append(data)
 
     def __str__(self):
         """Вызывается функциями str, print и format. Возвращает строковое представление объекта."""
-        result = []
-        current_node = self.head
-
-        for _ in range(self.len_ - 1):
-            result.append(current_node.value)
-            current_node = current_node.next
-
-        result.append(current_node.value)
-
-        return f"{result}"
+        return f"{self.to_list()}"
 
     def __repr__(self):
         """Метод должен возвращать строку, показывающую, как может быть создан экземпляр."""
-        return ""
+        return f"{type(self).__name__}({self.to_list()})"
 
     def __len__(self):
-        ...
+        return self.__len
 
-    def __getitem__(self, item: int) -> Any:
-        ...
+    def __getitem__(self, index: int) -> Any:
+        if self.__is_index(index):
+            if index < self.__len:
+                item = self.__get_node(index)
+                return item.value
+            else:
+                raise IndexError(f"Index {index} out of bounds")
+        else:
+            raise TypeError(f"Value {index} is not a valid index")
 
-    def __setitem__(self, key, value):
-        ...
+    def __setitem__(self, index, value):
+        if self.__is_index(index):
+            if index < self.__len:
+                item = self.__get_node(index)
+                item.value = value
+            else:
+                raise IndexError(f"Index {index} out of bounds")
+        else:
+            raise TypeError(f"Value {index} is not a valid index")
 
-    def append(self, value: Any):
+    def __get_node(self, index: int) -> Any:
+        current_node = self.head
+        for _ in range(index):
+            current_node = current_node.next
+        return current_node
+
+    def append(self, value: Any, increase_index=True):
         """Добавление элемента в конец связного списка"""
         append_node = self.Node(value)
         if self.head is None:
             self.head = append_node
+            self.tail = append_node
         else:
-            tail = self.head  # ToDo Завести атрибут self.tail, который будет хранить последний узел
-            for _ in range(self.len_ - 1):
-                tail = tail.next
-            self.__linked_nodes(tail, append_node)
+            self.__linked_nodes(self.tail, append_node)
+            self.tail = append_node
 
-        self.len_ += 1
-
-    @staticmethod
-    def __linked_nodes(left: Node, right: Optional[Node]) -> None:
-        left.next = right
+        if increase_index:
+            self.__len += 1
 
     def to_list(self) -> list:
-        ...
+        return [value for value in self]    # Итерирование мы уже реализовали с помощью магии, вот пусть оно и работает
 
     def insert(self, index: int, value: Any) -> None:
-        ...
+        # todo добавить проверку индекса
+        if index == 0:  # если первый элемент
+            to_insert = self.Node(value, self.head)
+            self.head = to_insert
+        if index == self.__len - 1:  # если последний элемент
+            self.append(value, False)
+        else:   # если любой другой элемент
+            left = self.__get_node(index - 1)
+            right = left.next
+            to_insert = self.Node(value, right)
+            left.next = to_insert
+        self.__len += 1
 
-    def clear(self) -> None:
-        ...
+    def clear(self) -> None:    # в питоне только так, за нас память чистит сборщик мусора
+        self.head = None
+        self.__len = 0
 
     def index(self, value: Any) -> int:
         ...
@@ -110,11 +133,27 @@ class LinkedList:
     def sort(self) -> None:
         ...
 
-    def is_iterable(self, data) -> bool:
+    @staticmethod
+    def __linked_nodes(left: Node, right: Optional[Node]) -> None:
+        left.next = right
+
+    @staticmethod
+    def __is_index(index) -> bool:
+        if not isinstance(index, int):
+            return False
+        return True
+
+    @staticmethod
+    def is_iterable(data) -> bool:
         """Метод для проверки является ли объект итерируемым"""
-        ...
+        try:
+            _ = (e for e in data)
+            return True
+        except TypeError:
+            return False
 
 
 if __name__ == '__main__':
-    ll = LinkedList([1, 2, 3, 4])
-    print(ll)
+    l1 = LinkedList("abcd")
+    print(l1)
+    print(repr(l1))
