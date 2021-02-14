2845bed00600c3f43165ddf98e016d1137b4fd1b
Dokuchaev <Kekofff>
2021-02-14 17:54:04 +0300

    dz_1 1) def append 2) def index 3) def remove 4) def sort 5) def is_iterable

--- lesson_3/linked_list.py
+++ lesson_3/linked_list.py
@@ -42,8 +42,9 @@ class LinkedList:
 
     def __init__(self, data: Sequence = None):
         """Конструктор связного списка"""
-        self.len_ = 0
+        self.__len = 0
         self.head = None  # Node
+        self.tail = None
 
         if data:  # ToDo Проверить, что объект итерируемый. Метод self.is_iterable
             for value in data:
@@ -51,70 +52,148 @@ class LinkedList:
 
     def __str__(self):
         """Вызывается функциями str, print и format. Возвращает строковое представление объекта."""
-        result = []
-        current_node = self.head
-
-        for _ in range(self.len_ - 1):
-            result.append(current_node.value)
-            current_node = current_node.next
-
-        result.append(current_node.value)
-
-        return f"{result}"
+        return f"{[value for value in self]}"
 
     def __repr__(self):
         """Метод должен возвращать строку, показывающую, как может быть создан экземпляр."""
-        return ""
+        return f"{type(self).__name__}({[value for value in self]})"
 
-    def __len__(self):
-        ...
+    def __len__(self) -> int:
+        return self.__len
+
+    def __step_by_step_on_nodes(self, index) -> 'Node':
+        if not isinstance(index, int):
+            raise TypeError()
+        if not 0 <= index < self.__len:
+            raise IndexError()
+        current_node = self.head
+        for _ in range(index):
+            current_node = current_node.next
+        return current_node
 
     def __getitem__(self, item: int) -> Any:
-        ...
+        current_node = self.__step_by_step_on_nodes(item)
+        return current_node.value
 
-    def __setitem__(self, key, value):
-        ...
+    def __setitem__(self, key: int, value: Any):
+        current_node = self.__step_by_step_on_nodes(key)
+        current_node.value = value
+
+    # def append_1(self, value: Any):
+    #     """Добавление элемента в конец связного списка"""
+    #     append_node = self.Node(value)
+    #     if self.head is None:
+    #         self.head = append_node
+    #     else:
+    #         tail = self.head  # ToDo Завести атрибут self.tail, который будет хранить последний узел
+    #         for _ in range(self.__len - 1):
+    #             tail = tail.next
+    #         self.__linked_nodes(tail, append_node)
+    #     self.__len += 1
 
     def append(self, value: Any):
-        """Добавление элемента в конец связного списка"""
         append_node = self.Node(value)
-        if self.head is None:
+        if self.head == None:
             self.head = append_node
-        else:
-            tail = self.head  # ToDo Завести атрибут self.tail, который будет хранить последний узел
-            for _ in range(self.len_ - 1):
-                tail = tail.next
-            self.__linked_nodes(tail, append_node)
+        if self.tail != None:
+            self.tail.next = append_node
+        self.tail = append_node
+        self.__len += 1
 
-        self.len_ += 1
 
     @staticmethod
     def __linked_nodes(left: Node, right: Optional[Node]) -> None:
         left.next = right
 
     def to_list(self) -> list:
-        ...
+        return [value for value in self]
 
     def insert(self, index: int, value: Any) -> None:
-        ...
+        if not isinstance(index, int):
+            raise TypeError()
+        if index == 0:
+            insert_node = self.Node(value)
+            self.__linked_nodes(insert_node, self.head)
+            self.head = insert_node
+            self.__len += 1
+        elif 1 <= index < self.__len:
+            prev_node = self.__step_by_step_on_nodes(index - 1)
+            current_node = prev_node.next
+            insert_node = self.Node(value, next_=current_node)
+            self.__linked_nodes(prev_node, insert_node)
+            self.__len += 1
+        elif index >= self.__len:
+            self.append(value)
 
     def clear(self) -> None:
-        ...
+        self.head = None
+        self.__len = 0
 
     def index(self, value: Any) -> int:
-        ...
+        current_node = self.head
+        for i in range(self.__len):
+            if str(current_node) == value:
+                return i
+            else:
+                current_node = current_node.next
+        raise ValueError(f'{value} is not in list')
 
     def remove(self, value: Any) -> None:
-        ...
+        current_index = self.index(value)
+        if current_index == 0:
+            self.head = self.__step_by_step_on_nodes(1)
+            self.__len -= 1
+        elif 1 <= current_index < self.__len:
+            prev_node = self.__step_by_step_on_nodes(current_index - 1)
+            prev_node.next = prev_node.next.next
+            # next_node = self.__step_by_step_on_nodes(current_index + 1)
+            # self.__linked_nodes(prev_node, next_node)
+            self.__len -= 1
+        elif current_index + 1 == self.__len:
+            prev_node = self.__step_by_step_on_nodes(current_index - 1)
+            prev_node.next = None
+            self.__len -= 1
 
     def sort(self) -> None:
-        ...
+        for i in range(self.__len):
+            current_index = 0
+            current_node = self.__step_by_step_on_nodes(current_index)
+            current_value = current_node.value
+            next_node = self.__step_by_step_on_nodes(current_index + 1)
+            next_value = next_node.value
+            for current_index in range(self.__len - 1):
+                if current_value > next_value:
+                    current_node.value = next_value
+                    next_node.value = current_value
+                current_node = self.__step_by_step_on_nodes(current_index)
+                current_value = current_node.value
+                next_node = self.__step_by_step_on_nodes(current_index + 1)
+                next_value = next_node.value
+            if current_value > next_value:
+                current_node.value = next_value
+                next_node.value = current_value
 
     def is_iterable(self, data) -> bool:
         """Метод для проверки является ли объект итерируемым"""
-        ...
+        if hasattr(data, '__iter__'):
+            return True
+        else:
+            raise AttributeError(f'{data.__class__.__name__} is not iterable')
 
 
 if __name__ == '__main__':
-    ll = LinkedList([1, 2, 3, 4])
-    print(ll)
+    # ll = LinkedList([1, 2, 3, 4])
+    # print(ll)
+
+    l1 = LinkedList('')
+    l1.append('8')
+    print(l1)
+    # l1.sort()
+    # print(l1)
+    # l1.insert(0, 'value')
+    # print(l1)
+    # l1.insert(len(l1)-1, 'new_value')
+    # print(l1)
+
+
+
